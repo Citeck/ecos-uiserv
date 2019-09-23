0c99ad2152126c61b051ff06476c72a7e8fecb70
Roman Makarskiy <Romanchabest55@gmail.com>
2019-09-23 13:31:46 +0300

    ECOSCOM-2640 - add action to uiserv

--- src/main/java/ru/citeck/ecos/uiserv/service/action/ActionRecords.java
+++ src/main/java/ru/citeck/ecos/uiserv/service/action/ActionRecords.java
@@ -1,9 +1,16 @@
 package ru.citeck.ecos.uiserv.service.action;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import ru.citeck.ecos.records2.RecordRef;
+import ru.citeck.ecos.uiserv.domain.ConfigDTO;
 import ru.citeck.ecos.uiserv.domain.action.dto.ActionDTO;
 import ru.citeck.ecos.uiserv.service.entity.AbstractEntityRecords;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Roman Makarskiy
@@ -17,6 +24,28 @@ public class ActionRecords extends AbstractEntityRecords<ActionDTO> {
     public ActionRecords(ActionService actionService) {
         setId(ID);
         this.entityService = actionService;
+    }
+
+    @Override
+    public List<ActionDTO> getValuesToMutate(List<RecordRef> records) {
+        List<ActionDTO> result = new ArrayList<>();
+        for (RecordRef recordRef : records) {
+            String id = recordRef.getId();
+            if (StringUtils.isBlank(id)) {
+                result.add(getEmpty());
+                continue;
+            }
+
+            Optional<ActionDTO> found = entityService.getById(id);
+            if (found.isPresent()) {
+                result.add(found.get());
+            } else {
+                ActionDTO dto = new ActionDTO();
+                dto.setId(id);
+                result.add(dto);
+            }
+        }
+        return result;
     }
 
     @Override
