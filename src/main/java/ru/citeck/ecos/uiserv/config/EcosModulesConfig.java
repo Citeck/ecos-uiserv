eb958e7884b796bdd2817ac77e3d785401bd4aff
Pavel Simonov <pavel.simonov@citeck.ru>
2019-10-04 12:59:56 +0700

    ECOSCOM-2717 - Add workflow deploying

--- src/main/java/ru/citeck/ecos/uiserv/config/EcosModulesConfig.java
+++ src/main/java/ru/citeck/ecos/uiserv/config/EcosModulesConfig.java
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import ru.citeck.ecos.apps.module.type.impl.dashboard.DashboardModule;
 import ru.citeck.ecos.apps.module.type.impl.form.FormModule;
 import ru.citeck.ecos.apps.queue.EcosAppQueues;
 import ru.citeck.ecos.apps.queue.ModulePublishMsg;
 import ru.citeck.ecos.apps.queue.ModulePublishResultMsg;
+import ru.citeck.ecos.uiserv.domain.DashboardDTO;
+import ru.citeck.ecos.uiserv.service.dashdoard.DashboardEntityService;
 import ru.citeck.ecos.uiserv.service.form.EcosFormModel;
 import ru.citeck.ecos.uiserv.service.form.EcosFormService;
 
@@ -18,20 +19,21 @@ import ru.citeck.ecos.uiserv.service.form.EcosFormService;
 @Configuration
 public class EcosModulesConfig {
 
-    private RestTemplate restTemplate;
     private EcosFormService formService;
     private ObjectMapper mapper = new ObjectMapper();
     private AmqpTemplate amqpTemplate;
 
-    public EcosModulesConfig(@Qualifier("microRestTemplate") RestTemplate restTemplate,
-                             EcosFormService formService,
-                             AmqpTemplate amqpTemplate) {
+    private DashboardEntityService dashboardEntityService;
+
+    public EcosModulesConfig(EcosFormService formService,
+                             AmqpTemplate amqpTemplate,
+                             DashboardEntityService dashboardEntityService) {
         this.formService = formService;
-        this.restTemplate = restTemplate;
         this.amqpTemplate = amqpTemplate;
+        this.dashboardEntityService = dashboardEntityService;
     }
 
-    //@RabbitListener(queues = {EcosAppQueues.PUBLISH_PREFIX + FormModule.TYPE})
+    @RabbitListener(queues = {EcosAppQueues.MODULE_TYPE_PUBLISH_PREFIX + FormModule.TYPE})
     public void deployForm(ModulePublishMsg msg) {
 
         ModulePublishResultMsg result = new ModulePublishResultMsg();
@@ -47,6 +49,34 @@ public class EcosModulesConfig {
 
             EcosFormModel formModel = mapper.readValue(formData, EcosFormModel.class);
             formService.save(formModel);
+
+            result.setSuccess(true);
+
+        } catch (Exception e) {
+
+            result.setMsg(e.getMessage());
+            result.setSuccess(false);
+        }
+
+        amqpTemplate.convertAndSend(EcosAppQueues.PUBLISH_RESULT_ID, result);
+    }
+
+    @RabbitListener(queues = {EcosAppQueues.MODULE_TYPE_PUBLISH_PREFIX + DashboardModule.TYPE})
+    public void deployDashboard(ModulePublishMsg msg) {
+
+        ModulePublishResultMsg result = new ModulePublishResultMsg();
+        result.setRevId(msg.getRevId());
+
+        try {
+
+            byte[] dashboardData = msg.getData();
+
+            if (dashboardData == null) {
+                throw new RuntimeException("Dashboard can't be deployed. Data is not received");
+            }
+
+            DashboardDTO dashboardDTO = mapper.readValue(dashboardData, DashboardDTO.class);
+            dashboardEntityService.update(dashboardDTO);
 
             result.setSuccess(true);
 
