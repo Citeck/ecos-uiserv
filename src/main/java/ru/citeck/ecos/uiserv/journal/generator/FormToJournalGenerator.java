761974054c597e9ea0e6b5bb35e3aebad1fbfef5
Oleg Raskin <raskin.o@yandex.ru>
2020-04-24 19:08:29 +0400

    ECOSCOM-3567 - Develop journal config builder based on form

---
+++ src/main/java/ru/citeck/ecos/uiserv/journal/generator/FormToJournalGenerator.java
@@ -0,0 +1,159 @@
+package ru.citeck.ecos.uiserv.journal.generator;
+
+import ecos.com.fasterxml.jackson210.databind.JsonNode;
+import ecos.com.fasterxml.jackson210.databind.node.ArrayNode;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
+import ru.citeck.ecos.commons.data.MLText;
+import ru.citeck.ecos.commons.data.ObjectData;
+import ru.citeck.ecos.commons.json.Json;
+import ru.citeck.ecos.uiserv.journal.dto.JournalColumnDto;
+import ru.citeck.ecos.uiserv.journal.dto.JournalDto;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Slf4j
+public class FormToJournalGenerator {
+
+    private static final String JOURNAL_PREFIX = "JOURNAL_";
+    private static final String JOURNAL_TEXT = "journal";
+
+    //todo: just for testing
+    @PostConstruct
+    public void init() throws IOException {
+        File file = ResourceUtils.getFile("classpath:ecos-app/module/ui/form/ecos-form.json");
+        String content = new String(Files.readAllBytes(file.toPath()));
+        this.read(content);
+    }
+
+    public JournalDto read(String content) {
+        JsonNode node = Json.getMapper().read(content);
+        if (node == null) {
+            throw new RuntimeException("Cannot get Journal from form. Cannot read JSON: " + content);
+        }
+
+        JournalDto journalDto = new JournalDto();
+
+        JsonNode idNode = node.path("id");
+        if (!idNode.isMissingNode()) {
+            journalDto.setId(JOURNAL_PREFIX + idNode.asText());
+        } else {
+            JsonNode formKeyNode = node.path("formKey");
+            if (!formKeyNode.isMissingNode()) {
+                journalDto.setId(JOURNAL_PREFIX + formKeyNode.asText());
+            } else {
+                throw new RuntimeException("Cannot get Journal from form. Cannot create 'id' for journal.");
+            }
+        }
+
+        JsonNode titleNode = node.path("title");
+        if (!titleNode.isMissingNode() && !titleNode.isNull()) {
+            journalDto.setName(new MLText(titleNode.asText() + " " + JOURNAL_TEXT));
+        }
+
+        ArrayNode components = node
+            .path("definition")
+            .withArray("components");
+        if (!components.isMissingNode() && !components.isMissingNode()) {
+            List<JournalColumnDto> columnsDtos = this.readColumns(components);
+            journalDto.setColumns(columnsDtos);
+        }
+
+        return journalDto;
+    }
+
+    private List<JournalColumnDto> readColumns(@NonNull ArrayNode componentsNode) {
+
+        List<JournalColumnDto> columnsDtos = new ArrayList<>();
+
+        for (JsonNode componentJsonNode : componentsNode) {
+
+            if (!isInputFormElement(componentJsonNode) || isButton(componentJsonNode)) {
+                continue;
+            }
+
+            if (hasInnerComponents(componentJsonNode)) {
+
+                ArrayNode innerColumnsNodes = componentJsonNode.withArray("columns");
+                for (JsonNode columnNode : innerColumnsNodes) {
+
+                    ArrayNode innerComponentsNodes = columnNode.withArray("components");
+                    if (!innerComponentsNodes.isMissingNode() && !innerComponentsNodes.isNull()) {
+                        columnsDtos.addAll(this.readColumns(innerComponentsNodes));
+                    }
+                }
+                continue;
+            }
+
+            JournalColumnDto columnDto = this.readColumn(componentJsonNode);
+            columnsDtos.add(columnDto);
+        }
+
+        return columnsDtos;
+    }
+
+    private JournalColumnDto readColumn(JsonNode componentNode) {
+
+        JournalColumnDto columnDto = new JournalColumnDto();
+
+        JsonNode labelNode = componentNode.path("label");
+        if (!labelNode.isMissingNode() && !labelNode.isNull()) {
+            columnDto.setLabel(new MLText(labelNode.asText()));
+        }
+
+        JsonNode protectedNode = componentNode.path("protected");
+        if (!protectedNode.isMissingNode() && !protectedNode.isNull()) {
+            columnDto.setEditable(!protectedNode.asBoolean());
+        }
+
+        JsonNode hiddenNode = componentNode.path("hidden");
+        if (!hiddenNode.isMissingNode() && !hiddenNode.isNull()) {
+            columnDto.setVisible(hiddenNode.asBoolean());
+        }
+
+        JsonNode attributesNode = componentNode.path("attributes");
+        if (!attributesNode.isMissingNode() && !attributesNode.isNull()) {
+            columnDto.setAttributes(new ObjectData(attributesNode));
+        }
+
+        return columnDto;
+    }
+
+    /**
+     * Check that received JsonNode has components inside
+     *
+     * @param componentNode
+     * @return boolean value of checking result
+     */
+    private boolean hasInnerComponents(JsonNode componentNode) {
+        return componentNode.get("columns") != null;
+    }
+
+    /**
+     * Check that received JsonNode has type "button"
+     *
+     * @param componentNode
+     * @return boolean value of checking result
+     */
+    private boolean isButton(JsonNode componentNode) {
+        return componentNode.path("type").asText().equals("button");
+    }
+
+    /**
+     * Check that received JsonNode is some kind of 'input'
+     *
+     * @param componentNode
+     * @return boolean value of checking result
+     */
+    private boolean isInputFormElement(JsonNode componentNode) {
+        return componentNode.path("input").asBoolean();
+    }
+}
