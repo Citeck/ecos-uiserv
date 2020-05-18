3bb3200a9fa1e6267d0b751856a8ebfeeabd61a7
Pavel Simonov <pavel.simonov@citeck.ru>
2020-05-18 14:20:14 +0700

    ECOSCOM-3442 - [UISERV] Move menu configs to a external table

--- src/test/java/ru/citeck/ecos/uiserv/service/menu/MenuServiceTest.java
+++ src/test/java/ru/citeck/ecos/uiserv/service/menu/MenuServiceTest.java
@@ -15,8 +15,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = Application.class)
 public class MenuServiceTest {
     @MockBean
     private MenuRepository menuRepository;
