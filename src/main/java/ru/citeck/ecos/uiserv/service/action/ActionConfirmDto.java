340234e865e83803983bca36fe4e9a31812d75d3
Pavel Simonov <pavel.simonov@citeck.ru>
2020-06-01 18:06:03 +0700

    ECOSCOM-3672 - Add confirm message for actions

--- src/main/java/ru/citeck/ecos/uiserv/service/action/ActionConfirmDto.java
+++ src/main/java/ru/citeck/ecos/uiserv/service/action/ActionConfirmDto.java
@@ -2,9 +2,11 @@ package ru.citeck.ecos.uiserv.service.action;
 
 import lombok.Data;
 import ru.citeck.ecos.commons.data.MLText;
+import ru.citeck.ecos.records2.RecordRef;
 
 @Data
 public class ActionConfirmDto {
     private MLText title;
     private MLText message;
+    private RecordRef formRef;
 }
