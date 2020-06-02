7fa1f6df7650b5114e83adf0c2427e2b6d767a14
Pavel Simonov <pavel.simonov@citeck.ru>
2020-06-02 12:44:49 +0700

    ECOSCOM-3636 - Journals 3.0, ecosType errors

--- src/main/java/ru/citeck/ecos/uiserv/journal/dto/legacy1/JournalConfigResp.java
+++ src/main/java/ru/citeck/ecos/uiserv/journal/dto/legacy1/JournalConfigResp.java
@@ -1,15 +1,25 @@
 package ru.citeck.ecos.uiserv.journal.dto.legacy1;
 
 import lombok.Data;
+import ru.citeck.ecos.records2.graphql.meta.annotation.MetaAtt;
 
 import java.util.List;
 import java.util.Map;
 
 @Data
 public class JournalConfigResp {
+
     private String id;
     private String sourceId;
     private JournalMeta meta;
     private List<Column> columns;
     private Map<String, String> params;
+
+    @MetaAtt(".disp")
+    public String getDisplayName() {
+        if (meta == null) {
+            return "Journal";
+        }
+        return meta.getTitle();
+    }
 }
