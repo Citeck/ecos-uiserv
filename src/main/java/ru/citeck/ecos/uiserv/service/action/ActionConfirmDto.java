28e69e5c37c3bb25318e3d0bf0678faa96b6a5df
Pavel Simonov <pavel.simonov@citeck.ru>
2020-06-19 11:43:22 +0700

    ECOSUI-176 - Confirm action config: add ability to map form attributes

--- src/main/java/ru/citeck/ecos/uiserv/service/action/ActionConfirmDto.java
+++ src/main/java/ru/citeck/ecos/uiserv/service/action/ActionConfirmDto.java
@@ -4,9 +4,12 @@ import lombok.Data;
 import ru.citeck.ecos.commons.data.MLText;
 import ru.citeck.ecos.records2.RecordRef;
 
+import java.util.Map;
+
 @Data
 public class ActionConfirmDto {
     private MLText title;
     private MLText message;
     private RecordRef formRef;
+    private Map<String, String> attributesMapping;
 }
