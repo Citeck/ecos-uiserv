69d447d74d65ddb3fd5d9907e7c156cd9facd7c9
Pavel Simonov <pavel.simonov@citeck.ru>
2020-08-20 15:19:21 +0700

    ECOSUI-431 - Menu cache (/ v2) does not update when changing case type

--- src/main/java/ru/citeck/ecos/uiserv/app/application/api/records/MetaRecordAttsProvider.java
+++ src/main/java/ru/citeck/ecos/uiserv/app/application/api/records/MetaRecordAttsProvider.java
@@ -1,8 +1,15 @@
 package ru.citeck.ecos.uiserv.app.application.api.records;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
+import ru.citeck.ecos.records2.RecordConstants;
+import ru.citeck.ecos.records2.RecordsService;
 import ru.citeck.ecos.records2.graphql.meta.value.MetaField;
+import ru.citeck.ecos.records2.predicate.PredicateService;
+import ru.citeck.ecos.records2.request.query.RecordsQuery;
+import ru.citeck.ecos.records2.request.query.SortBy;
+import ru.citeck.ecos.records2.rest.RemoteRecordsUtils;
 import ru.citeck.ecos.records2.source.dao.local.meta.MetaAttributesSupplier;
 import ru.citeck.ecos.records2.source.dao.local.meta.MetaRecordsDaoAttsProvider;
 import ru.citeck.ecos.uiserv.domain.journal.service.JournalService;
@@ -12,16 +19,28 @@ import javax.annotation.PostConstruct;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Component
 @RequiredArgsConstructor
 public class MetaRecordAttsProvider implements MetaAttributesSupplier {
 
     private static final String ATT_MENU_CACHE_KEY = "menu-cache-key";
+    private static final long TYPES_CHECK_INTERVAL = 5000;
 
     private final JournalService journalService;
     private final MenuService menuService;
     private final MetaRecordsDaoAttsProvider provider;
+    private final RecordsService recordsService;
+
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    private long lastTypesChecked = 0;
+    private volatile Future<?> typesCheckFuture = null;
+
+    private String lastModifiedType = "";
 
     @PostConstruct
     void init() {
@@ -33,11 +52,51 @@ public class MetaRecordAttsProvider implements MetaAttributesSupplier {
         return Collections.singletonList(ATT_MENU_CACHE_KEY);
     }
 
+    private void updateTypesChangedTime() {
+
+        RecordsQuery query = new RecordsQuery();
+        query.setSourceId("emodel/type");
+        query.addSort(new SortBy(RecordConstants.ATT_MODIFIED, false));
+        query.setMaxItems(1);
+        query.setLanguage(PredicateService.LANGUAGE_PREDICATE);
+
+        RemoteRecordsUtils.runAsSystem(() -> {
+            recordsService.queryRecord(query, Collections.singletonList(RecordConstants.ATT_MODIFIED))
+                .ifPresent(meta -> {
+                    String modified = meta.get(RecordConstants.ATT_MODIFIED).asText();
+                    if (StringUtils.isNotBlank(modified)) {
+                        lastModifiedType = modified;
+                    }
+                });
+            return null;
+        });
+    }
+
+    private void syncTypesChangedTime() {
+
+        long currentTime = System.currentTimeMillis();
+        if (typesCheckFuture == null && ((currentTime - lastTypesChecked) > TYPES_CHECK_INTERVAL)) {
+            synchronized (this) {
+                if (typesCheckFuture == null) {
+                    lastTypesChecked = currentTime;
+                    typesCheckFuture = executorService.submit(() -> {
+                        updateTypesChangedTime();
+                        typesCheckFuture = null;
+                    });
+                }
+            }
+        }
+    }
+
     @Override
     public Object getAttribute(String attribute, MetaField field) {
+
+        syncTypesChangedTime();
+
         return Objects.hash(
             journalService.getLastModifiedTimeMs(),
-            menuService.getLastModifiedTimeMs()
+            menuService.getLastModifiedTimeMs(),
+            lastModifiedType
         );
     }
 }
