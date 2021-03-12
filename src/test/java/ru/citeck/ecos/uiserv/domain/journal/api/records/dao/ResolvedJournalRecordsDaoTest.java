550541eee5cf57ad42c35fccafae8cb469c74c45
Zaytsev Aleksey <aleksey.zaytsev@citeck.ru>
2021-03-12 18:05:21 +0700

    ECOSUI-993 - Group actions were disseapeared Added tests

--- src/test/java/ru/citeck/ecos/uiserv/domain/journal/api/records/dao/ResolvedJournalRecordsDaoTest.java
+++ src/test/java/ru/citeck/ecos/uiserv/domain/journal/api/records/dao/ResolvedJournalRecordsDaoTest.java
@@ -1,22 +1,30 @@
 package ru.citeck.ecos.uiserv.domain.journal.api.records.dao;
 
 import ecos.com.fasterxml.jackson210.databind.JsonNode;
+import org.apache.commons.lang3.LocaleUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.util.reflection.FieldSetter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import ru.citeck.ecos.commons.data.MLText;
+import ru.citeck.ecos.commons.data.ObjectData;
 import ru.citeck.ecos.commons.json.Json;
+import ru.citeck.ecos.model.lib.attributes.dto.AttributeType;
 import ru.citeck.ecos.records2.RecordRef;
 import ru.citeck.ecos.records2.source.dao.local.InMemRecordsDao;
+import ru.citeck.ecos.records3.RecordsService;
+import ru.citeck.ecos.records3.record.atts.dto.RecordAtts;
+import ru.citeck.ecos.records3.record.dao.AbstractRecordsDao;
 import ru.citeck.ecos.uiserv.Application;
 import ru.citeck.ecos.uiserv.domain.ecostype.dto.EcosTypeInfo;
-import ru.citeck.ecos.uiserv.domain.ecostype.service.EcosTypeService;
 import ru.citeck.ecos.uiserv.domain.journal.api.records.ResolvedJournalRecordsDao;
+import ru.citeck.ecos.uiserv.domain.journal.dto.JournalColumnDef;
+import ru.citeck.ecos.uiserv.domain.journal.dto.resolve.ResolvedColumnDef;
 import ru.citeck.ecos.uiserv.domain.journal.dto.resolve.ResolvedJournalDef;
 import ru.citeck.ecos.uiserv.domain.journal.repo.JournalEntity;
 import ru.citeck.ecos.uiserv.domain.journal.repo.JournalRepository;
-import ru.citeck.ecos.uiserv.domain.journal.service.mapper.JournalMapper;
 
 import java.io.File;
 import java.util.Arrays;
@@ -24,30 +32,34 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ActiveProfiles("test")
 @SpringBootTest(classes = Application.class)
 public class ResolvedJournalRecordsDaoTest {
 
     @Autowired
-    EcosTypeService ecosTypeService;
-
-    @Autowired
-    JournalMapper journalMapper;
-
-    @Autowired
-    ResolvedJournalRecordsDao testDao;
+    private ResolvedJournalRecordsDao testDao;
 
     @Autowired
     private JournalRepository journalRepository;
 
     @Autowired
-    InMemRecordsDao<EcosTypeInfo> typesSyncRecordsDao;
+    private InMemRecordsDao<EcosTypeInfo> typesSyncRecordsDao;
 
-    final static String TEST_SIMPLE_JOURNAL_ENTITY_JSON = "rJournalDaoTest/simple-journal-entity.json";
-    final static String ECOS_TYPE_INFO_JSON = "rJournalDaoTest/ecos-type-info.json";
-    final static String TEST_ACTIONS_DEF_JSON = "rJournalDaoTest/actionsDef.json";
-    final static String TEST_ACTIONS_JSON = "rJournalDaoTest/actions.json";
+    private JournalEntity journalEntity;
+
+    private final static String TEST_SIMPLE_JOURNAL_ENTITY_JSON = "ResolvedJournalRecordsDaoTest/simple-journal-entity.json";
+    private final static String ECOS_TYPE_INFO_JSON = "ResolvedJournalRecordsDaoTest/ecos-type-info.json";
+    private final static String TEST_ACTIONS_DEF_JSON = "ResolvedJournalRecordsDaoTest/actionsDef.json";
+    private final static String TEST_ACTIONS_JSON = "ResolvedJournalRecordsDaoTest/actions.json";
+    private final static String TEST_RECORDS_COL_ANSWER_JSON = "ResolvedJournalRecordsDaoTest/records-col-answer.json";
+
+    private final String actions = getSomethingFromFile(TEST_ACTIONS_JSON, JsonNode.class).toString();
+    private final String actionsDef = getSomethingFromFile(TEST_ACTIONS_DEF_JSON, JsonNode.class).toString();
 
     private <T> T getSomethingFromFile(String filePath, Class<T> tClass){
         filePath = getClass().getClassLoader().getResource(filePath).getFile();
@@ -59,12 +71,12 @@ public class ResolvedJournalRecordsDaoTest {
         EcosTypeInfo ecosTypeInfo = getSomethingFromFile(ECOS_TYPE_INFO_JSON, EcosTypeInfo.class);
         typesSyncRecordsDao.setRecord(ecosTypeInfo.getId(), ecosTypeInfo);
         journalRepository.deleteAll();
+
+        journalEntity = getSomethingFromFile(TEST_SIMPLE_JOURNAL_ENTITY_JSON, JournalEntity.class);
     }
 
     @Test
     void actionsFromTypeIsNullWithActionsDef() {
-        JournalEntity journalEntity = getSomethingFromFile(TEST_SIMPLE_JOURNAL_ENTITY_JSON, JournalEntity.class);
-        String actionsDef = getSomethingFromFile(TEST_ACTIONS_DEF_JSON, JsonNode.class).toString();
         journalEntity.setActionsFromType(null);
         journalEntity.setActionsDef(actionsDef);
         journalEntity.setActions("");
@@ -80,7 +92,6 @@ public class ResolvedJournalRecordsDaoTest {
 
     @Test
     void actionsFromTypeIsNullWithoutActions() {
-        JournalEntity journalEntity = getSomethingFromFile(TEST_SIMPLE_JOURNAL_ENTITY_JSON, JournalEntity.class);
         journalEntity.setActionsFromType(null);
         journalEntity.setActionsDef("");
         journalEntity.setActions("");
@@ -97,8 +108,6 @@ public class ResolvedJournalRecordsDaoTest {
 
     @Test
     void actionsFromTypeIsNullWithActions() {
-        JournalEntity journalEntity = getSomethingFromFile(TEST_SIMPLE_JOURNAL_ENTITY_JSON, JournalEntity.class);
-        String actions = getSomethingFromFile(TEST_ACTIONS_JSON, JsonNode.class).toString();
         journalEntity.setActionsFromType(null);
         journalEntity.setActionsDef("");
         journalEntity.setActions(actions);
@@ -116,8 +125,6 @@ public class ResolvedJournalRecordsDaoTest {
 
     @Test
     void actionsFromTypeIsFalseWithActionsDef() {
-        JournalEntity journalEntity = getSomethingFromFile(TEST_SIMPLE_JOURNAL_ENTITY_JSON, JournalEntity.class);
-        String actionsDef = getSomethingFromFile(TEST_ACTIONS_DEF_JSON, JsonNode.class).toString();
         journalEntity.setActionsFromType(false);
         journalEntity.setActionsDef(actionsDef);
         journalEntity.setActions("");
@@ -133,7 +140,6 @@ public class ResolvedJournalRecordsDaoTest {
 
     @Test
     void actionsFromTypeIsFalseWithoutActions() {
-        JournalEntity journalEntity = getSomethingFromFile(TEST_SIMPLE_JOURNAL_ENTITY_JSON, JournalEntity.class);
         journalEntity.setActionsFromType(false);
         journalEntity.setActionsDef("");
         journalEntity.setActions("");
@@ -147,8 +153,6 @@ public class ResolvedJournalRecordsDaoTest {
 
     @Test
     void actionsFromTypeIsFalseWithActions() {
-        JournalEntity journalEntity = getSomethingFromFile(TEST_SIMPLE_JOURNAL_ENTITY_JSON, JournalEntity.class);
-        String actions = getSomethingFromFile(TEST_ACTIONS_JSON, JsonNode.class).toString();
         journalEntity.setActionsFromType(false);
         journalEntity.setActionsDef("");
         journalEntity.setActions(actions);
@@ -166,8 +170,6 @@ public class ResolvedJournalRecordsDaoTest {
 
     @Test
     void actionsFromTypeIsTrueWithActionsDef() {
-        JournalEntity journalEntity = getSomethingFromFile(TEST_SIMPLE_JOURNAL_ENTITY_JSON, JournalEntity.class);
-        String actionsDef = getSomethingFromFile(TEST_ACTIONS_DEF_JSON, JsonNode.class).toString();
         journalEntity.setActionsFromType(true);
         journalEntity.setActionsDef(actionsDef);
         journalEntity.setActions("");
@@ -185,7 +187,6 @@ public class ResolvedJournalRecordsDaoTest {
 
     @Test
     void actionsFromTypeIsTrueWithoutActions() {
-        JournalEntity journalEntity = getSomethingFromFile(TEST_SIMPLE_JOURNAL_ENTITY_JSON, JournalEntity.class);
         journalEntity.setActionsFromType(true);
         journalEntity.setActionsDef("");
         journalEntity.setActions("");
@@ -202,8 +203,6 @@ public class ResolvedJournalRecordsDaoTest {
 
     @Test
     void actionsFromTypeIsTrueWithActions() {
-        JournalEntity journalEntity = getSomethingFromFile(TEST_SIMPLE_JOURNAL_ENTITY_JSON, JournalEntity.class);
-        String actions = getSomethingFromFile(TEST_ACTIONS_JSON, JsonNode.class).toString();
         journalEntity.setActionsFromType(true);
         journalEntity.setActionsDef("");
         journalEntity.setActions(actions);
@@ -218,5 +217,48 @@ public class ResolvedJournalRecordsDaoTest {
         );
         List<RecordRef> actionsList = recordAtts.getJournalDef().getActions();
         assertThat(actionsList).isEqualTo(expected);
+    }
+
+    @Test
+    void columnTestEval() throws Exception {
+        journalRepository.save(journalEntity);
+
+        RecordAtts colsAtts = new RecordAtts("alfresco/@");
+        JsonNode testData = getSomethingFromFile(TEST_RECORDS_COL_ANSWER_JSON, JsonNode.class);
+        colsAtts.setAttributes(ObjectData.create(testData));
+
+        // Mock recordsService
+        RecordsService recordsServiceMock = mock(RecordsService.class);
+        when(recordsServiceMock.getAtts(eq(RecordRef.valueOf("alfresco/@")), anyMap()))
+            .thenReturn(colsAtts);
+
+        // Set recordsServiceMock in testDao
+        FieldSetter.setField(testDao, AbstractRecordsDao.class.getDeclaredField("recordsService") ,recordsServiceMock);
+
+        ResolvedJournalDef recordAtts = (ResolvedJournalDef) testDao.getRecordAtts(journalEntity.getExtId());
+        List<ResolvedColumnDef> invoke = recordAtts.getColumnsEval().invoke();
+        JournalColumnDef testCol = invoke.get(2).getColumn();
+
+        List<Object> checkVals = Arrays.asList(
+            testCol.getId(),
+            testCol.getAttribute(),
+            testCol.getName(),
+            testCol.getType(),
+            testCol.getEditor().getType(),
+            testCol.getEditor().getConfig().get("journalId").asText()
+        );
+
+        List<Object> expectedVals = Arrays.asList(
+            "test-column",
+            "test-column",
+            new MLText()
+                .withValue(LocaleUtils.toLocale("ru"), "Тестовый столбец молодец")
+                .withValue(LocaleUtils.toLocale("en"), "Testovii stolbec molodec"),
+            AttributeType.ASSOC,
+            "journal",
+            "simple-journal"
+        );
+
+        assertThat(checkVals).isEqualTo(expectedVals);
     }
 }
