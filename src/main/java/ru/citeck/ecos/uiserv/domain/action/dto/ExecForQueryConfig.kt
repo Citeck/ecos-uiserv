e79cb2259c9a8b2e2086d1d7af22c1af8b893286
Pavel Simonov <pavel.simonov@citeck.ru>
2022-06-14 18:01:05 +0700

    ECOSUI-1734 - GroupAction. Add preAction module for query mode

--- src/main/java/ru/citeck/ecos/uiserv/domain/action/dto/ExecForQueryConfig.kt
+++ src/main/java/ru/citeck/ecos/uiserv/domain/action/dto/ExecForQueryConfig.kt
@@ -5,14 +5,7 @@ import ru.citeck.ecos.records2.RecordConstants
 import ru.citeck.ecos.records3.record.atts.schema.annotation.AttName
 
 data class ExecForQueryConfig(
-    val execAsForRecords: Boolean? = null,
-    /**
-     * Workaround to avoid simplification of data for DTO with one field
-     * This problem will fix in next release and this field won't be required
-     */
-    @JsonIgnore
-    @AttName(RecordConstants.ATT_NULL)
-    val nullField: String? = null
+    val execAsForRecords: Boolean? = null
 ) {
     companion object {
         @JvmField
