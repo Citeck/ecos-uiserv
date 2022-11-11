de970c8d054399c13e2859d795527199137a36ac
pavel.marchuk <pavel.marchuk@citeck.ru>
2022-11-11 12:25:05 +0700

    ECOSCOM-4864: Journal: Fixing of filter by text in the 'assoc' column. Add ability to customize innerQuery

---
+++ src/main/java/ru/citeck/ecos/uiserv/domain/journal/dto/SearchByText.kt
@@ -0,0 +1,57 @@
+package ru.citeck.ecos.uiserv.domain.journal.dto
+
+import ecos.com.fasterxml.jackson210.databind.annotation.JsonDeserialize
+import ru.citeck.ecos.commons.data.ObjectData
+import ru.citeck.ecos.commons.json.serialization.annotation.IncludeNonDefault
+
+@IncludeNonDefault
+@JsonDeserialize(builder = SearchByText.Builder::class)
+data class SearchByText(
+    //todo use when RecordsQuery will include @IncludeNonDefault annotation (ecos-records:3.51.1)
+//    val innerQuery: ru.citeck.ecos.records3.record.dao.query.dto.query.RecordsQuery,
+    val innerQuery: ObjectData,
+    val innerQueryAttribute: String
+) {
+    companion object {
+        @JvmField
+        val EMPTY = SearchByText(
+            ObjectData.create("""{"sourceId":"","query":"","page":{"maxItems":20}}"""),
+            ""
+        )
+    }
+
+    fun copy(): Builder {
+        return Builder(this)
+    }
+
+    fun copy(builder: Builder.() -> Unit): SearchByText {
+        val builderObj = Builder(this)
+        builder.invoke(builderObj)
+        return builderObj.build()
+    }
+
+    class Builder() {
+
+        var innerQuery: ObjectData = ObjectData.create()
+        var innerQueryAttribute: String = ""
+
+        constructor(base: SearchByText) : this() {
+            this.innerQuery = ObjectData.deepCopyOrNew(base.innerQuery)
+            this.innerQueryAttribute = base.innerQueryAttribute
+        }
+
+        fun withInnerQuery(innerQuery: ObjectData?): Builder {
+            this.innerQuery = innerQuery ?: ObjectData.create()
+            return this
+        }
+
+        fun withInnerQueryAttribute(innerQueryAttribute: String?): Builder {
+            this.innerQueryAttribute = innerQueryAttribute ?: ""
+            return this
+        }
+
+        fun build(): SearchByText {
+            return SearchByText(innerQuery, innerQueryAttribute)
+        }
+    }
+}
