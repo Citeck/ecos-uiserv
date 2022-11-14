4ee14bf0033cb7b51cf6028aa3af621911a2cc5e
pavel.marchuk <pavel.marchuk@citeck.ru>
2022-11-14 17:53:04 +0700

    ECOSCOM-4864: fixed remark

--- src/main/java/ru/citeck/ecos/uiserv/domain/journal/dto/SearchByText.kt
+++ src/main/java/ru/citeck/ecos/uiserv/domain/journal/dto/SearchByText.kt
@@ -7,17 +7,29 @@ import ru.citeck.ecos.commons.json.serialization.annotation.IncludeNonDefault
 @IncludeNonDefault
 @JsonDeserialize(builder = SearchByText.Builder::class)
 data class SearchByText(
-    //todo use when RecordsQuery will include @IncludeNonDefault annotation (ecos-records:3.51.1)
-//    val innerQuery: ru.citeck.ecos.records3.record.dao.query.dto.query.RecordsQuery,
+    /**
+     * Query for preprocessing by column on the client. Analogue of ru.citeck.ecos.records3.record.dao.query.dto.query.RecordsQuery structure.
+     * Default: {"sourceId":"","query":"","page":{"maxItems":20}}
+     * Need to use RecordsQuery type, when ecos-records:3.51.1 will be used.
+     * In ecos-records:3.51.1 RecordsQuery include annotation @IncludeNonDefault, for compact serialization to json.
+     */
     val innerQuery: ObjectData,
     val innerQueryAttribute: String
 ) {
     companion object {
-        @JvmField
-        val EMPTY = SearchByText(
-            ObjectData.create("""{"sourceId":"","query":"","page":{"maxItems":20}}"""),
-            ""
-        )
+        val EMPTY = create {}
+
+        @JvmStatic
+        fun create(): Builder {
+            return Builder()
+        }
+
+        @JvmStatic
+        fun create(builder: Builder.() -> Unit): SearchByText {
+            val builderObj = Builder()
+            builder.invoke(builderObj)
+            return builderObj.build()
+        }
     }
 
     fun copy(): Builder {
