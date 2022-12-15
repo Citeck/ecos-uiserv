503a3205faea3d72ce94d4f88742af60b8714682
Pavel Simonov <pavel.simonov@citeck.ru>
2022-12-15 09:55:39 +0700

    ECOSCOM-4921 - Journals. Add default fields on creation

--- src/main/java/ru/citeck/ecos/uiserv/domain/journal/service/provider/TypeJournalsProvider.kt
+++ src/main/java/ru/citeck/ecos/uiserv/domain/journal/service/provider/TypeJournalsProvider.kt
@@ -1,8 +1,11 @@
 package ru.citeck.ecos.uiserv.domain.journal.service.provider
 
 import org.springframework.stereotype.Component
+import ru.citeck.ecos.commons.data.MLText
 import ru.citeck.ecos.commons.data.entity.EntityWithMeta
+import ru.citeck.ecos.context.lib.i18n.I18nContext
 import ru.citeck.ecos.model.lib.type.service.utils.TypeUtils
+import ru.citeck.ecos.records3.record.atts.schema.ScalarType
 import ru.citeck.ecos.uiserv.domain.journal.dto.JournalColumnDef
 import ru.citeck.ecos.uiserv.domain.journal.dto.JournalDef
 import ru.citeck.ecos.uiserv.domain.journal.service.JournalService
@@ -31,18 +34,30 @@ class TypeJournalsProvider(
 
     private fun createJournalDef(typeDef: TypeDef): JournalDef {
 
+        val columns = mutableListOf<JournalColumnDef>()
+        columns.add(
+            JournalColumnDef.create()
+                .withId(ScalarType.DISP.mirrorAtt)
+                .withName(
+                    MLText(
+                        I18nContext.RUSSIAN to "Имя",
+                        I18nContext.ENGLISH to "Name"
+                    )
+                )
+                .build()
+        )
+        columns.addAll(typeDef.model.attributes.map {
+            JournalColumnDef.create()
+                .withId(it.id)
+                .withType(it.type)
+                .withName(it.name)
+                .build()
+        })
+
         return JournalDef.create {
             withName(typeDef.name)
             withTypeRef(TypeUtils.getTypeRef(typeDef.id))
-            withColumns(
-                typeDef.model.attributes.map {
-                    JournalColumnDef.create()
-                        .withId(it.id)
-                        .withType(it.type)
-                        .withName(it.name)
-                        .build()
-                }
-            )
+            withColumns(columns)
         }
     }
 
