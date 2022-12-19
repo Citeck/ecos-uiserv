5b928dd5e84099c796f6ccd7e07cf1769512fce8
Pavel Simonov <pavel.simonov@citeck.ru>
2022-12-19 10:54:47 +0700

    ECOSCOM-4921 - Journals. Add default fields on creation

--- src/main/java/ru/citeck/ecos/uiserv/domain/journal/service/provider/TypeJournalsProvider.kt
+++ src/main/java/ru/citeck/ecos/uiserv/domain/journal/service/provider/TypeJournalsProvider.kt
@@ -5,6 +5,7 @@ import ru.citeck.ecos.commons.data.MLText
 import ru.citeck.ecos.commons.data.entity.EntityWithMeta
 import ru.citeck.ecos.context.lib.i18n.I18nContext
 import ru.citeck.ecos.model.lib.type.service.utils.TypeUtils
+import ru.citeck.ecos.records2.RecordConstants
 import ru.citeck.ecos.records3.record.atts.schema.ScalarType
 import ru.citeck.ecos.uiserv.domain.journal.dto.JournalColumnDef
 import ru.citeck.ecos.uiserv.domain.journal.dto.JournalDef
@@ -35,18 +36,22 @@ class TypeJournalsProvider(
     private fun createJournalDef(typeDef: TypeDef): JournalDef {
 
         val columns = mutableListOf<JournalColumnDef>()
-        columns.add(
-            JournalColumnDef.create()
-                .withId(ScalarType.DISP.mirrorAtt)
-                .withName(
-                    MLText(
-                        I18nContext.RUSSIAN to "Имя",
-                        I18nContext.ENGLISH to "Name"
+
+        val attributes = typeDef.model.attributes
+        if (attributes.none { it.id == "name" }) {
+            columns.add(
+                JournalColumnDef.create()
+                    .withId(ScalarType.DISP.mirrorAtt)
+                    .withName(
+                        MLText(
+                            I18nContext.RUSSIAN to "Имя",
+                            I18nContext.ENGLISH to "Name"
+                        )
                     )
-                )
-                .build()
-        )
-        columns.addAll(typeDef.model.attributes.map {
+                    .build()
+            )
+        }
+        columns.addAll(attributes.map {
             JournalColumnDef.create()
                 .withId(it.id)
                 .withType(it.type)
