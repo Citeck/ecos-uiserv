198589086a3d10bf722e73b41cbb74809366859d
Pavel Simonov <pavel.simonov@citeck.ru>
2025-11-25 11:44:07 +0700

    ECOSCOM-5905 - WS. Template creation error. Release 2.32.1

--- src/main/java/ru/citeck/ecos/uiserv/domain/workspace/api/GetWorkspaceArtifactsForTemplateWebExecutor.kt
+++ src/main/java/ru/citeck/ecos/uiserv/domain/workspace/api/GetWorkspaceArtifactsForTemplateWebExecutor.kt
@@ -2,6 +2,7 @@ package ru.citeck.ecos.uiserv.domain.workspace.api
 
 import org.springframework.stereotype.Component
 import ru.citeck.ecos.commons.utils.ZipUtils
+import ru.citeck.ecos.context.lib.auth.AuthContext
 import ru.citeck.ecos.uiserv.domain.workspace.service.WorkspaceUiService
 import ru.citeck.ecos.webapp.api.web.executor.EcosWebExecutor
 import ru.citeck.ecos.webapp.api.web.executor.EcosWebExecutorReq
@@ -18,10 +19,15 @@ class GetWorkspaceArtifactsForTemplateWebExecutor(
 
     override fun execute(request: EcosWebExecutorReq, response: EcosWebExecutorResp) {
 
+        if (AuthContext.isNotRunAsSystem()) {
+            error("Permission denied")
+        }
+
         val req = request.getBodyReader().readDto(GetWorkspaceArtifactsReq::class.java)
+        val respData = workspaceUiService.getWsArtifactsForTemplate(req.workspace)
 
         response.getBodyWriter().getOutputStream().use {
-            ZipUtils.writeZip(workspaceUiService.getWsArtifactsForTemplate(req.workspace), it)
+            ZipUtils.writeZip(respData, it)
         }
     }
 
